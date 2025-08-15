62c045b2e425f99552028182e6ee95dbda6d4d5a
Leonel Sanches da Silva <leonel@designliquido.com.br>
2025-08-14 19:40:31 -0700

    `palavras-reservadas` agora faz parte do Lexador.

--- fontes/index.ts
+++ fontes/index.ts
@@ -3,6 +3,5 @@ export * from './comandos';
 export * from './construtos';
 export * from './interfaces';
 export * from './lexador';
-export * from './palavras-reservadas';
 export * from './tipos-de-simbolos/lincones';
 export * from './tradutor';
