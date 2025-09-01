d0019eb1948d5607059ed2ca1af1bada8371c12a
Leonel Sanches da Silva <leonel@designliquido.com.br>
2025-09-01 13:33:05 -0700

    `iniciar` deve suportar `Promise`.

--- fontes/interfaces/tecnologia-lincones-interface.ts
+++ fontes/interfaces/tecnologia-lincones-interface.ts
@@ -2,7 +2,7 @@ import { Comando } from "../comandos";
 import { RetornoComandoInterface } from "./retorno-comando-interface";
 
 export interface TecnologiaLinconesInterface {
-    iniciar(caminho: string): void;
+    iniciar(caminho: string): Promise<void>;
     executar(_: any, sentencaLincones: string, parametros: any[]): Promise<RetornoComandoInterface[]>;
     executarComando(comando: Comando): Promise<RetornoComandoInterface[]>;
 }
