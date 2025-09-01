41dd060df765f264c98ef19618401cd7d9c4d07e
Leonel Sanches da Silva <leonel@designliquido.com.br>
2025-09-01 13:11:42 -0700

    Adicionando `executar` à interface `TecnologiaLinconesInterface`, por exigência de Liquido.

--- fontes/interfaces/tecnologia-lincones-interface.ts
+++ fontes/interfaces/tecnologia-lincones-interface.ts
@@ -3,5 +3,6 @@ import { RetornoComandoInterface } from "./retorno-comando-interface";
 
 export interface TecnologiaLinconesInterface {
     iniciar(caminho: string): void;
+    executar(_: any, sentencaLincones: string, parametros: any[]): Promise<RetornoComandoInterface[]>;
     executarComando(comando: Comando): Promise<RetornoComandoInterface[]>;
 }
